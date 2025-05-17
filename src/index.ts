38be7ea805cd7c1d31ba9690295c6cc027f02e1e
Aniruddha Sil <welcometohellv@gmail.com>
2025-05-17 17:29:47 +0530

    trial route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Elysia } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(8000)
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .get('/hello', () => 'Hello World')
+  .listen(8000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
