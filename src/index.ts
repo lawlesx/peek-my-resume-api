c136ce8f2d93a0408789038e7e87e258f997561f
Aniruddha Sil <welcometohellv@gmail.com>
2025-05-18 11:36:54 +0530

    feat: :zap: added route to upload pdf

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import { Elysia } from 'elysia'
+import { existsSync, mkdirSync } from 'fs'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(8000)
+const uploadsDir = './.uploads'
 
-console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
+if (!existsSync(uploadsDir)) {
+  mkdirSync(uploadsDir)
+}
+
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia!')
+  .post('/upload', async ({ request }) => {
+    const form = await request.formData()
+    const file = form.get('resume') as File
+
+    if (!file || !(file instanceof File)) {
+      return { error: 'No file uploaded' }
+    }
+
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    await Bun.write(`${uploadsDir}/${file.name}`, buffer)
+
+    return { message: `Uploaded ${file.name}` }
+  })
+  .listen(8000)
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
